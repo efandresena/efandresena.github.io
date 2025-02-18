22e46bde9824ddef69d069755f39d3eb1fe583ea
efandresena <fandresenarandriamanantena98@gmail.com>
2025-02-18 13:48:01 +0300

    Added documents, search functionality, and social media links

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("searchInput");
+  const documentList = document.getElementById("documentList");
+
+  searchInput.addEventListener("input", function () {
+    const filter = searchInput.value.toLowerCase();
+    const items = documentList.getElementsByTagName("li");
+
+    for (let i = 0; i < items.length; i++) {
+      const itemText = items[i].textContent || items[i].innerText;
+      if (itemText.toLowerCase().includes(filter)) {
+        items[i].style.display = "";
+      } else {
+        items[i].style.display = "none";
+      }
+    }
+  });
+});
